985ac47b58722b9fa080c91a55adaf590e82349a
Aleksandr Katyushnyj <a-katushnii@yandex.ru>
2023-04-25 22:04:38 +0300

    Выполнил задание

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Author bulgakov = new Author("Михаил Булгаков");
+        Author king = new Author("Стивен Кинг");
+        Book masterAndMargarita = new Book("Мастер и Маргарита", bulgakov, 1967);
+        Book greenMile = new Book("Зеленая миля", king, 1996);
+        System.out.println("masterAndMargarita.getYear() = " + masterAndMargarita.getYear());
+        masterAndMargarita.setYear(1968);
+        System.out.println("masterAndMargarita.getYear() = " + masterAndMargarita.getYear());
     }
 }
