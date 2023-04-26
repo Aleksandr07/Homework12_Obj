a7e2bd262bd709baa08bce4021df76710968f44b
Aleksandr Katyushnyj <a-katushnii@yandex.ru>
2023-04-26 21:30:07 +0300

    Выполнил задание из урока "Методы объектов"

--- src/Author.java
+++ src/Author.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class Author {
     private String author;
 
@@ -7,5 +9,23 @@ public class Author {
 
     public String getAuthor() {
         return author;
+    }
+
+    @Override
+    public String toString() {
+        return author;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Author author1 = (Author) o;
+        return Objects.equals(author, author1.author);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(author);
     }
 }
